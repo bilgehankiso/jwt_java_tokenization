12443dec6eba8e24b56e3ae777377e2c6a966e2c
Bilgehan Kışo <73483481+bilgehankiso@users.noreply.github.com>
2025-03-06 15:25:55 +0300

    Update SecurityConfig.java

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -11,9 +11,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // CSRF korumasını kapat
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // Tüm isteklere izin ver
+                        .anyRequest().permitAll() 
                 );
 
         return http.build();
